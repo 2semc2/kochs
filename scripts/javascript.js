eb3723679c6911ca2dabe069cc57c6d7c1cc4130
PatrickKM <32448400+PatrickKM@users.noreply.github.com>
2018-02-19 16:21:53 +0100

    Ændringer på HTML, CSS, JavaScript, ny video

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,25 +1,25 @@
 // RULES FOR PROGRESS INDICATOR
-window.onscroll = function () {
+window.onscroll = function () { // Der gives en funktion når man scroller
     myFunction()
 };
 
-function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
+function myFunction() { // Variabler bliver skrevet ind
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop; // en for scrolltop, hvor man kan finde dens vertikale position
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight; // en for scrollHeight, hvor man finder hele sidens højde
+    var scrolled = (winScroll / height) * 100;  // en hvor man udregner hvor på siden man er 
+    document.getElementById("myBar").style.width = scrolled + "%"; // til sidst har vi en javascript som skal style myBar med vores variabel og vise hvor langt fremme på siden man er via. en horisontal indikator
 }
 
 // RULES FOR TEXT FADE IN FOR P TAGS
-window.addEventListener("load",
+window.addEventListener("load", // Når siden loades
     function () {
-        window.addEventListener("scroll",
+        window.addEventListener("scroll", // skal functionen scroll
             function () {
-                var nodes = document.getElementsByTagName("P");
+                var nodes = document.getElementsByTagName("P"); // finde p tags
                 for (var i = 0; i < nodes.length; i++) {
                     if (nodes[i].getBoundingClientRect().top >= 0 &&
-                        nodes[i].getBoundingClientRect().bottom < window.innerHeight) {
-                        nodes[i].setAttribute("class", "fading");
+                        nodes[i].getBoundingClientRect().bottom < window.innerHeight) { // indenfor skærmens område
+                        nodes[i].setAttribute("class", "fading"); // og "fade" teksten ind
                     } else {
                         nodes[i].setAttribute("class", "");
                     }
@@ -27,6 +27,7 @@ window.addEventListener("load",
             })
     })
 
+/*
 // RULES FOR AUDIO TAG
 window.addEventListener("scroll",
     function () {
@@ -45,4 +46,4 @@ window.addEventListener("scroll",
 function elFllVsbl(el) {
     return (el.getBoundingClientRect().top>0 &&
            el.getBoundingClientRect().bottom<window.innerHeight)
-}
+} */
